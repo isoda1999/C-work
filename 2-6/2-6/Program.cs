ef044d655bc5781ee9d181e557e7f63e18f30841
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-10-13 18:26:19 +0900

    2

---
+++ 2-6/2-6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("年齢？");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("おおよその日数"+ age * 365);
+        }
+       
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(x);
+            Console.WriteLine(x * x);
+            Console.WriteLine(x * x * x);
+        }
+    }
+}
