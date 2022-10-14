5c24c00f5bed84e09f48d7300dc634ce69c7986f
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-10-14 16:13:47 +0900

    3－2追加

---
+++ 3-2/3-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y = 0;
+            x= int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            if (x > y)
+                Console.WriteLine(x);
+          
+        }
+}
