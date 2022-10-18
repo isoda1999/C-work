13c7f0d567a3330c144abe0a7393337efa323aeb
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-10-18 17:37:28 +0900

    4－12

---
+++ 4-12/4-12/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int total =0;
+while (total<100){
+    total += int.Parse(Console.ReadLine());
+    Console.WriteLine("合計" + total);
+}
