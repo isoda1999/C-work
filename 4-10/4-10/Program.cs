759d98ba079c6e003254bad85739fa8aa6634ad8
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-10-18 15:52:27 +0900

    4－10

--- 4-10/4-10/Program.cs
+++ 4-10/4-10/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int count = int.Parse(Console.ReadLine());
+for (int i = 0; i < count; i++){
+    Console.Write("*");
+}
