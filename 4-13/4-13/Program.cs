ede642fe84757650afbf62045e5b340f4bf5375d
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-10-20 12:14:25 +0900

    4－13

---
+++ 4-13/4-13/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+    int strike = 0;
+    int ball = 0;
+    while (strike < 3 && ball < 4) {
+        Console.Write("ストライク=1かボール=2で入力して下さい ");
+        int judgement = int.Parse(Console.ReadLine());
+
+        if (judgement == 1){
+            strike++;
+        }
+        else if (judgement == 2)
+            ball++;
+        }
+Console.WriteLine("ストライク" + strike+":"+"ボール" + ball) ;
+
+
