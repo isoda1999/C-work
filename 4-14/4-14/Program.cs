ff1bed282be3d887e7dfea159d52546be9a687f0
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-10-20 14:49:43 +0900

    ４－１４

---
+++ 4-14/4-14/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int strike = 0;
+int ball = 0;
+//各球種の入力回数のMAXを決める
+while (strike < 3 && ball < 4){
+    //各球種の入力欄を作る
+    Console.Write("ストライク=1,ボール=2,ファール=3で入力して下さい ");
+    int judgement = int.Parse(Console.ReadLine());
+    //whileで指定した回数までカウントを繰り返し、上限に達したらそれまで入力された値を表示する
+    if (judgement == 1){
+        strike++;
+    }else if (judgement == 2){
+        ball++;
+    }else if (judgement == 3 && strike < 2){
+    }   
+        Console.WriteLine("ストライク" + strike + ":" + "ボール" + ball);   
+}
