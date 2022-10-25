03a1ab87349885281e9e6c8ed4c62a32227791cd
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-10-25 15:38:40 +0900

    4-19

---
+++ 4-19/4-19/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int sum = 0;
+int count = 0;
+
+while (true ){
+    int num = int.Parse(Console.ReadLine());
+
+    if (num == 0) {
+        break;
+    }
+        sum += num;
+        count++;
+    }
+
+
+Console.WriteLine("平均は" + (sum / count));
