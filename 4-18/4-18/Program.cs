ce80f8b67460c49f291be4648395628fa35c7af2
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-10-25 13:34:32 +0900

    4－18

--- 4-18/4-18/Program.cs
+++ 4-18/4-18/Program.cs
@@ -1,20 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 int sum = 0;
-while (true)
-{
-    int num = int.Parse(Console.ReadLine());
-    Console.WriteLine(int.Parse(Console.ReadLine()));
+int num = int.Parse(Console.ReadLine());
+sum += num;
+while (true){
     if (num == 0)
     {
         break;
+       
 
     }
-
-    sum += num;
-
-
     Console.WriteLine(num);
     Console.WriteLine("合計は" + sum);
 }
-            
