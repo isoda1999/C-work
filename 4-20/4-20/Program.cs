acf3e01584a389f0bd798928fafcfe2c4ea78018
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-10-25 18:37:34 +0900

    4-20

---
+++ 4-20/4-20/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Drawing;
+
+int size = int.Parse(Console.ReadLine());
+ size = 0;
+String dollars = "";
+for (int i = 0; i < size; i++){
+    dollars += "$";
+    Console.Write(dollars);
+}
