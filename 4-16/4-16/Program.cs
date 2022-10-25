218e5c8d409b33e21720c1b1fb7babe206588ce0
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-10-25 12:24:12 +0900

    4-15,16

---
+++ 4-16/4-16/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+  bool IsPrime(long x) { 
+ x = int.Parse(Console.ReadLine());
+    if (x < 2) return false;
+    if (x == 2) return true;
+    if (x % 2 == 0) return false;
+
+    double sqrtX = Math.Sqrt(x);
+    for (long i = 3; i < sqrtX; i += 2){
+        if (x % i == 0) return false;
+    }
+    return true;
+}
---
+++ ４－１５/４－１５/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+public class Prime
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("{0} = {1}", n, string.Join(" x ", PrimeFactors(n)));
+    }
+
+    public static IEnumerable<int> PrimeFactors(int n)
+    {
+        int i = 2;
+        int tmp = n;
+
+        while (i * i <= n)
+        {
+            if (tmp % i == 0)
+            {
+                tmp /= i;
+                yield return i;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        if (tmp != 1) yield return tmp;{
+            Console.WriteLine();
+        }
+    }
+}
