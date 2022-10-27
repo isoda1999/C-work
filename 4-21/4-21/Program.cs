907c05a497d8fe463a96f8727aa9432ad6b6e0db
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-10-27 17:52:32 +0900

    4-21　□を出したところ

---
+++ 4-21/4-21/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Drawing;
+Console.WriteLine("hellow world");
+//コンソールに入力された値をとってくる
+int n = int.Parse(Console.ReadLine());
+//String型の変数Sizeに空文字を入れて初期化
+String dollars = "";
+//入力された数だけ文字列増やす処理をSizeにつける
+for (int i = 0; i < n; i++)
+{
+    dollars = "";
+    //順に＄を増やす
+    for (int j =0; j < n; j++)
+    {
+       
+        dollars += "$";
+    }
+    Console.WriteLine(dollars);
+}
+
