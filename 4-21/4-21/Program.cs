2e8d3bd8e86d36eb477faba58a53436f0badc053
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-10-28 10:46:34 +0900

    4-21

--- 4-21/4-21/Program.cs
+++ 4-21/4-21/Program.cs
@@ -1,20 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Drawing;
+using System.Text;
+
 Console.WriteLine("hellow world");
+
 //コンソールに入力された値をとってくる
-int n = int.Parse(Console.ReadLine());
-//String型の変数Sizeに空文字を入れて初期化
-String dollars = "";
+//int n = int.Parse(Console.ReadLine());
+int n = 5;
+
 //入力された数だけ文字列増やす処理をSizeにつける
 for (int i = 0; i < n; i++)
 {
-    dollars = "";
+    //String型の変数Sizeに空文字を入れて初期化
+    String dollars = "";
+
     //順に＄を増やす
-    for (int j =0; j < n; j++)
+    for (int j = 0; j < n; j++)
     {
-       
-        dollars += "$";
+        if (j == i)
+        {
+            dollars += "$";
+        }
+        else if (j == (n - i - 1))
+        {
+            dollars += "$";
+        }
+        else
+        {
+            dollars += " ";
+        }
     }
     Console.WriteLine(dollars);
 }
-
