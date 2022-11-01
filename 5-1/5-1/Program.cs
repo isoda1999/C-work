b2c941d3822d0cd4a950d9f74e23053e4d5c1ddd
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-11-01 14:27:05 +0900

    5-1　完成

--- 5-1/5-1/Program.cs
+++ 5-1/5-1/Program.cs
@@ -1,15 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
 Console.WriteLine("Hello, World!");
-int y = 0;
-//配列の要素数と入力の回数を10回に増やす
+//配列の宣言・宣言
+int[] x = new int[10];
+int n = 0;
+//入力された値を10回取得する
 for (int i = 0; i < 10; i++)
 {
-    int[] x = { y };
-
-    y = int.Parse(Console.ReadLine());
+    n = int.Parse(Console.ReadLine());
 }
-//要素を×２して出力する
-for (int i = 0; i < 11; i++)
+//10個の要素を×2する
+for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine(+i * 2);
+    Console.WriteLine("2倍された値"+(n + i) * 2);
 }
