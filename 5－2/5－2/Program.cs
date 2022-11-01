fc5e029272c3b150f5714c0e1974eff092e5dea2
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-11-01 15:44:00 +0900

    5－2

---
+++ 5－2/5－2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//配列の宣言・要素数の初期化
+int[] x = new int[10];
+//入力された値を10回取得する
+for (int i = 0; i < 10; i++)
+{
+    x[i] = int.Parse(Console.ReadLine());
+}
+//10個の要素を×2する
+for (int i = 9; i > 0; i--)
+{
+    Console.WriteLine("逆順=" + x[i]);
+}
