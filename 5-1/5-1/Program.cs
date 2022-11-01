6d4f564f3278b104fd57b6e2697e4fb98fcefa32
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-11-01 12:36:56 +0900

    5-1

---
+++ 5-1/5-1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int y = 0;
+//配列の要素数と入力の回数を10回に増やす
+for (int i = 0; i < 10; i++)
+{
+    int[] x = { y };
+
+    y = int.Parse(Console.ReadLine());
+}
+//要素を×２して出力する
+for (int i = 0; i < 11; i++)
+{
+    Console.WriteLine(+i * 2);
+}
