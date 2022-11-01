3aefce77ab8387f888ee07dd7662737fde7de2db
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-11-01 15:36:11 +0900

    5-1完成

--- 5-1/5-1/Program.cs
+++ 5-1/5-1/Program.cs
@@ -2,16 +2,16 @@
 using System.ComponentModel;
 
 Console.WriteLine("Hello, World!");
-//配列の宣言・宣言
+//配列の宣言・要素数の初期化
 int[] x = new int[10];
-int n = 0;
+
 //入力された値を10回取得する
 for (int i = 0; i < 10; i++)
 {
-    n = int.Parse(Console.ReadLine());
+    x[i] = int.Parse(Console.ReadLine());
 }
 //10個の要素を×2する
 for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine("2倍された値"+(n + i) * 2);
+    Console.WriteLine("2倍された値=" + (x[i]) * 2);
 }
