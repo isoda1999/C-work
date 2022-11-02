aea1e3ab78abcbd95e2e11d89da663316d6c96c0
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-11-02 10:40:06 +0900

    5-3 完成

---
+++ 5－3/5－3/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//配列の宣言・要素数の初期化
+int[] x = new int[10];
+//入力された値を10回取得する
+for (int i = 0; i < 10; i++)
+{
+    x[i] = int.Parse(Console.ReadLine());
+}
+//10個の要素をする
+for (int i = 0; i < 10; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine("偶数=" + (x[i]));
+    }
+}
+
+for (int i = 0; i < 10; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine("奇数=" + (x[i]));
+    }
+}
