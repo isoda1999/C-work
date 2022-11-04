8ab4db4b4b0ce3a4c5e1e24dcba3a138c92e3e08
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-11-04 14:12:32 +0900

    5－6

--- 5-6/5-6/Program.cs
+++ 5-6/5-6/Program.cs
@@ -1,13 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
 Console.WriteLine("Hello, World!");
-int kuku[] = new int[9, 9];
+// 値を格納する配列 kuku を宣言する
+int[,] kuku = new int[9, 9];
 
 // 配列 kuku に値を代入する
-
-for (int i = 0; i < 10; i++)
+for (int i = 1; i <= 9; i++)
 {
-    for (int j = 0; j < 10; j++)
+    for (int j = 1; j <= 9; j++)
+    {
+        Console.WriteLine(i * j);
+        kuku[i - 1, j - 1] = i * j;
+    }
+}
+
+// 結果を出力する
+for (int i = 0; i < 9; i++)
+{
+    for (int j = 0; j < 9; j++)
+    {
         Console.Write(" {0,2}", kuku[i, j]);
+    }
 
     Console.WriteLine();
 }
