6bf2f171a762cb2bb80a1bcbacd6ff40b07b961c
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-11-04 17:39:19 +0900

    5－7

---
+++ 5－7/5－7/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Hello, World!");
+// 値を格納する配列 kuku を宣言する
+int[,] kuku = new int[9, 9];
+
+// 配列 kuku に値を代入する
+for (int i = 1; i <= 9; i++)
+{
+    for (int j = 1; j <= 9; j++)
+    {
+        kuku[i - 1, j - 1] = i * j;
+    }
+}
+// 結果を出力する
+int sum = 0;
+int x = 0;
+int y = 0;
+
+Console.WriteLine("左辺");
+x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("右辺");
+y = int.Parse(Console.ReadLine());
+
+int result = kuku[x - 1, y - 1];
+Console.WriteLine(result);
