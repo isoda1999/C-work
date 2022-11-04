9bb4743cb2821b0d0512c8193bb1403f83d1fe34
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-11-04 11:32:05 +0900

    ５－４

---
+++ 5-4/5-4/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+//配列の宣言・要素数の初期化
+int[] x = new int[10];
+int sum = 0;
+//入力された値を10回取得する
+int i = 0;
+for (i = 0; i < 10; i++)
+{
+    x[i] = int.Parse(Console.ReadLine());
+
+    sum += x[i];
+
+    if (sum >= 100)
+    {
+        Console.WriteLine(sum);
+        break;
+    }
+}
+for (i = 0; i < 10; i++)
+    if (i < 10)
+    {
+        Console.WriteLine("入力された値" + x[i]);
+    }
---
+++ 5-6/5-6/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int kuku[] = new int[9, 9];
+
+// 配列 kuku に値を代入する
+
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 10; j++)
+        Console.Write(" {0,2}", kuku[i, j]);
+
+    Console.WriteLine();
+}
